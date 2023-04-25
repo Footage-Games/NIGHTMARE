f2ac6f65094f5de04a7007e39c429d6ae51e4fa9
Footage-Games <131554618+Footage-Games@users.noreply.github.com>
2023-04-24 21:21:21 -0300

    Update main.py

--- source/main.py
+++ source/main.py
@@ -1,1 +1,16 @@
-print("hello")
+import pygame
+
+pygame.init()
+
+Width-Screen = 600
+Height-Screen = 300
+
+Screen = pygame.display.set_mode((Width-Screen, Heighr-Screen))
+
+class Game:
+    def __init__(self):
+        Running = True
+        while Running = True:
+            for Event in pygame.event.get():
+                if Event.type == QUIT:
+                    pygame.quit()
